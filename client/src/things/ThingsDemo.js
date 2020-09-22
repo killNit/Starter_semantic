a1c66f1bb62086d4022865324d969ae6be00b9b0
James Yeates <jimibue@gmail.com>
2020-09-22 11:45:54 -0600

    first accetable starting point

--- client/src/things/ThingsDemo.js
+++ client/src/things/ThingsDemo.js
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const ThingsDemo = () => <div>Things Demo</div>;
+const ThingsDemo = () => {
+  const [things, setThings] = useState([]);
+
+  // [] as second arg for componentdidmount
+  // ()=>{} this is a unnamed function in this example it is a callback
+  // function(){}
+  useEffect(() => {
+    console.log("in useEFect");
+    //component did mount logic
+
+    //axios.get() return a promise which resolves or rejects
+    // resolve =>triggers .then( (thingReturnedinResolve)=>{} )
+    // reject =>triggers .catch( ()=>{})
+    axios
+      .get("/api/things")
+      .then((response) => {
+        setThings(response.data);
+      })
+      .catch((error) => {
+        alert("error in retrieving things");
+      });
+  }, []);
+  return (
+    <>
+      <div>
+        {things.map((t) => (
+          <p key={t.id}>{t.name}</p>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default ThingsDemo;
