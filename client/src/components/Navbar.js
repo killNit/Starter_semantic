727e3fd6e8c05b2c955bdcd62ec5f71cc5850767
James Yeates <jimibue@gmail.com>
2020-09-22 11:10:57 -0600

    styles

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,15 +4,19 @@ import { Link } from "react-router-dom";
 // For Basic setup only please change
 const NavBar = () => {
   return (
-    <div>
-      <Link to="/">
-        <p>Home</p>
-      </Link>
-      <Link to="/thingsDemo">
-        <p>Things</p>
-      </Link>
+    <div style={styles.navbar}>
+      <Link to="/">Home</Link>
+      <span style={{ marginRight: "10px" }}></span>
+      <Link to="/thingsDemo">Things</Link>
     </div>
   );
+};
+
+const styles = {
+  navbar: {
+    background: "black",
+    padding: "10px",
+  },
 };
 
 export default NavBar;
